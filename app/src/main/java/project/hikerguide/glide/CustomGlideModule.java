3f5c747710e407e3aa11e3a506c4badef27d7194
Alvin <amagh1990@gmail.com>
2017-07-17 17:00:57 -0700

    Configure Glide library
    Added CustomGlideModule to access generated API.

---
+++ app/src/main/java/project/hikerguide/glide/CustomGlideModule.java
@@ -0,0 +1,12 @@
+package project.hikerguide.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Alvin on 7/17/2017.
+ */
+
+@GlideModule
+public class CustomGlideModule extends AppGlideModule {
+}
