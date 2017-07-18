fa997e7a119be29c68cbbdcd9e8c54b050d0b3ce
Alvin <amagh1990@gmail.com>
2017-07-18 00:07:35 -0700

    Add data model for Areas

---
+++ app/src/main/java/project/hikerguide/models/Area.java
@@ -0,0 +1,40 @@
+package project.hikerguide.models;
+
+import android.database.Cursor;
+
+import project.hikerguide.data.GuideContract;
+
+/**
+ * Created by Alvin on 7/18/2017.
+ */
+
+public class Area {
+    public long id;
+    public String name;
+
+    public Area() {}
+
+    public Area(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Creates an Area Object using values descirbed in a Cursor
+     *
+     * @param cursor    Cursor describing an Area
+     * @return Area with values described in the Cursor
+     */
+    public Area createAreaFromCursor(Cursor cursor) {
+        // Index the columns of the Cursor
+        int idxId = cursor.getColumnIndex(GuideContract.AreaEntry._ID);
+        int idxName = cursor.getColumnIndex(GuideContract.AreaEntry.NAME);
+
+        // Retrieve the values from the Cursor
+        long id = cursor.getLong(idxId);
+        String name = cursor.getString(idxName);
+
+        // Create a new Area with the values
+        return new Area(id, name);
+    }
+}
