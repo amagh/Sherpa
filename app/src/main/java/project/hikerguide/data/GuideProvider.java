ac02dafd6685a42f3c1dfbd904fc86f464a3ba26
Alvin <amagh1990@gmail.com>
2017-07-17 20:07:05 -0700

    Add GuideProvider class
    Will use Schematic to generating the actual ContentProvider. Also
    includes an entry in AndroidManifest to enable the provider.

---
+++ app/src/main/java/project/hikerguide/data/GuideProvider.java
@@ -0,0 +1,181 @@
+package project.hikerguide.data;
+
+import android.net.Uri;
+
+import net.simonvt.schematic.annotation.ContentProvider;
+import net.simonvt.schematic.annotation.ContentUri;
+import net.simonvt.schematic.annotation.InexactContentUri;
+import net.simonvt.schematic.annotation.TableEndpoint;
+
+/**
+ * ContentProvider framework to be used by Schematic to generate the ContentProvider
+ */
+
+@ContentProvider(
+        authority = GuideProvider.AUTHORITY,
+        database = GuideDatabase.class)
+public class GuideProvider {
+    // ** Constants ** //
+    public static final String AUTHORITY = "project.hikerguide.data";
+    static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    /**
+     * Builds a URI from the BASE_CONTENT_URI by appending paths to it
+     *
+     * @param paths    Array of paths to append
+     * @return URI with the paths appended to the BASE_CONTENT_URI
+     */
+    private static Uri buildUri(String... paths) {
+        // Get a reference to the Uri.Builder
+        Uri.Builder builder = BASE_CONTENT_URI.buildUpon();
+
+        // For each path in the parameters, append the path to the BASE_CONTENT_URI
+        for (String path : paths) {
+            builder.appendPath(path);
+        }
+
+        // Build and return the URI
+        return builder.build();
+    }
+
+    /**
+     * Defines the paths used to access the data in the database
+     */
+    interface Path {
+        String GUIDES       = "guides";
+        String TRAILS       = "trails";
+        String AUTHORS      = "authors";
+        String SECTIONS     = "sections";
+        String AREAS        = "areas";
+    }
+
+    /**
+     * URIs for accessing guide data
+     */
+    @TableEndpoint(table = GuideDatabase.GUIDES)
+    public static class Guides {
+
+        @ContentUri(
+                path = Path.GUIDES,
+                type = "vnd.android.cursor.dir/guides",
+                defaultSort = GuideContract.GuideEntry.DATE_ADDED + " DESC")
+        public static final Uri CONTENT_URI = buildUri(Path.GUIDES);
+
+        @InexactContentUri(
+                path = Path.GUIDES + "/#",
+                name = "GUIDE_DETAILS",
+                type = "vnd.android.cursor.item/guides",
+                whereColumn = GuideContract.GuideEntry._ID,
+                pathSegment = 1,
+                join = "JOIN " + GuideDatabase.GUIDES + " ON " +
+                        GuideDatabase.GUIDES + "." + GuideContract.GuideEntry.TRAIL_ID + " = "  +
+                        GuideDatabase.TRAILS + "." + GuideContract.TrailEntry._ID               +
+                        " JOIN " + GuideDatabase.GUIDES + " ON "                                +
+                        GuideDatabase.GUIDES + "." + GuideContract.GuideEntry.AUTHOR_ID + " = " +
+                        GuideDatabase.AUTHORS + "." + GuideContract.AuthorEntry._ID
+        )
+        public static Uri withId(long id) {
+            return CONTENT_URI.buildUpon().appendPath(Long.toString(id)).build();
+        }
+
+        @InexactContentUri(
+                path = Path.GUIDES + "/" + Path.TRAILS + "/#",
+                name = "GUIDES_FOR_TRAIL",
+                type = "vnd.android.cursor.dir/guides",
+                whereColumn = GuideContract.GuideEntry.TRAIL_ID,
+                pathSegment = 2)
+        public static Uri forTrail(long id) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(Path.TRAILS)
+                    .appendPath(Long.toString(id))
+                    .build();
+        }
+
+        @InexactContentUri(
+                path = Path.GUIDES + "/" + Path.AUTHORS + "/#",
+                name = "GUIDE_WITH_AUTHOR_ID",
+                type = "vnd.android.cursor.dir/guides",
+                whereColumn = GuideContract.GuideEntry.AUTHOR_ID,
+                pathSegment = 2)
+        public static Uri fromAuthor(long id) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(Path.AUTHORS)
+                    .appendPath(Long.toString(id))
+                    .build();
+        }
+    }
+
+    /**
+     * URIs for accessing trail data
+     */
+    @TableEndpoint(table = GuideDatabase.TRAILS)
+    public static class Trails {
+
+        @ContentUri(
+                path = Path.TRAILS,
+                type = "vnd.android.cursor.dir/trails")
+        public static final Uri CONTENT_URI = buildUri(Path.TRAILS);
+
+        @InexactContentUri(
+                path = Path.TRAILS + "/" + Path.AREAS + "/#",
+                name = "PATHS_FOR_AREA",
+                type = "vnd.android.cursor.dir/trails",
+                whereColumn = GuideContract.TrailEntry.AREA_ID,
+                pathSegment = 2)
+        public static Uri forArea(long id) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(Path.AREAS)
+                    .appendPath(Long.toString(id))
+                    .build();
+        }
+    }
+
+    /**
+     * URIs for accessing author data
+     */
+    @TableEndpoint(table = GuideDatabase.AUTHORS)
+    public static class Authors {
+
+        @ContentUri(
+                path = Path.AUTHORS,
+                type = "vnd.android.cursor.dir/authors")
+        public static final Uri CONTENT_URI = buildUri(Path.AUTHORS);
+    }
+
+    /**
+     * URIs for accessing section data
+     */
+    @TableEndpoint(table = GuideDatabase.SECTIONS)
+    public static class Sections {
+
+        @ContentUri(
+                path = Path.SECTIONS,
+                type = "vnd.android.cursor.dir/sections")
+        public static final Uri CONTENT_URI = buildUri(Path.SECTIONS);
+
+        @InexactContentUri(
+                path = Path.SECTIONS + "/" + Path.GUIDES + "/#",
+                name = "SECTIONS_FOR_GUIDE",
+                type = "vnd.android.cursor.dir/sections",
+                whereColumn = GuideContract.SectionEntry.GUIDE_ID,
+                pathSegment = 3)
+        public static Uri forGuide(long id) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(Path.GUIDES)
+                    .appendPath(Long.toString(id))
+                    .build();
+        }
+    }
+
+    /**
+     * URIs for accessing area data
+     */
+    @TableEndpoint(table = GuideDatabase.AREAS)
+    public static class Areas {
+
+        @ContentUri(
+                path = Path.AREAS,
+                type = "vnd.android.cursor.dir/areas")
+        public static final Uri CONTENT_URI = buildUri(Path.AREAS);
+    }
+}
