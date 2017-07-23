0f207e1c27f36644374912355635bbcd31e9f63d
Alvin <amagh1990@gmail.com>
2017-07-23 00:41:36 -0700

    Add GuideDetailsAdapter for managing Views in GuideActivity
    Currently only handles GuideViewModel because it's all that has been
    created. Will add handling of Sections and Author when their ViewModels
    have been added.

---
+++ app/src/main/java/project/hikerguide/ui/adapters/GuideDetailsAdapter.java
@@ -0,0 +1,119 @@
+package project.hikerguide.ui.adapters;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import project.hikerguide.R;
+import project.hikerguide.databinding.ListItemGuideDetailsBinding;
+import project.hikerguide.models.datamodels.Author;
+import project.hikerguide.models.datamodels.Guide;
+import project.hikerguide.models.datamodels.Section;
+import project.hikerguide.models.datamodels.abstractmodels.BaseModel;
+import project.hikerguide.models.viewmodels.GuideViewModel;
+
+/**
+ * Created by Nocturna on 7/22/2017.
+ */
+
+public class GuideDetailsAdapter extends RecyclerView.Adapter {
+    // ** Constants ** //
+    private static final int GUIDE_VIEW_TYPE = 0;
+    private static final int SECTION_VIEW_TYPE = 1;
+    private static final int SECTION_IMAGE_VIEW_TYPE = 2;
+    private static final int AUTHOR_VIEW_TYPE = 3;
+
+    // ** Member Variables ** //
+    private BaseModel[] mModels;
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        int layoutId;
+
+         switch (viewType) {
+             case GUIDE_VIEW_TYPE:
+                 layoutId = R.layout.list_item_guide_details;
+                 break;
+
+             case SECTION_VIEW_TYPE:
+                 layoutId = R.layout.list_item_section_text;
+                 break;
+
+             case SECTION_IMAGE_VIEW_TYPE:
+                 layoutId = R.layout.list_item_section_image;
+                 break;
+
+             case AUTHOR_VIEW_TYPE:
+                 layoutId = R.layout.list_item_author;
+                 break;
+
+             default: throw new UnsupportedOperationException("Unknown view type: " + viewType);
+         }
+
+        return new GuideDetailsViewHolder(DataBindingUtil.inflate(inflater, layoutId, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mModels != null) {
+            return mModels.length;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (position == 0) {
+            // The first item is reserved for the section details
+            return GUIDE_VIEW_TYPE;
+        } else if (position == mModels.length) {
+            // The last item is for the author's details
+            return AUTHOR_VIEW_TYPE;
+        } else if (!((Section) mModels[position]).hasImage){
+            // If the Section does not have an image, inflate the layout without an image
+            return SECTION_VIEW_TYPE;
+        } else {
+            // Otherwise, the Section does have an image and requires the alternate layout
+            return SECTION_IMAGE_VIEW_TYPE;
+        }
+    }
+
+    public void swapModels(BaseModel[] models) {
+        mModels = models;
+
+        notifyDataSetChanged();
+    }
+
+    public class GuideDetailsViewHolder extends RecyclerView.ViewHolder {
+        private ViewDataBinding mBinding;
+
+        public GuideDetailsViewHolder(ViewDataBinding binding) {
+            super(binding.getRoot());
+
+            mBinding = binding;
+        }
+
+        public void bind(int position) {
+            BaseModel model = mModels[position];
+
+            if (model instanceof Guide) {
+                ((ListItemGuideDetailsBinding) mBinding).setVm(
+                        new GuideViewModel(mBinding.getRoot().getContext(), (Guide) model));
+            } else if (model instanceof Section) {
+
+            } else if (model instanceof Author) {
+
+            }
+        }
+    }
+}
