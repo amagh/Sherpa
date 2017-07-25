ea3476e1a9d2bf0f527b3a6fe096273f4f9f6ba5
Alvin <amagh1990@gmail.com>
2017-07-25 16:34:06 -0700

    Change the way distance and elevation is formatted
    Was previously using DecimalFormat and a constant to display the data.
    Changed it so that it uses the already exists String resources.

--- app/src/main/java/project/hikerguide/mpandroidchart/DistanceAxisFormatter.java
+++ app/src/main/java/project/hikerguide/mpandroidchart/DistanceAxisFormatter.java
@@ -7,31 +7,27 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.text.DecimalFormat;
 
+import project.hikerguide.R;
+
 /**
  * Created by Alvin on 7/25/2017.
  */
 
 public class DistanceAxisFormatter implements IAxisValueFormatter {
-    // ** Constants ** //
-    private static String MILES_LABEL = " mi";
-    private static String KILOMETERS_LABEL = " km";
-
     // ** Member Variables ** //
     Context mContext;
-    DecimalFormat mFormat;
 
     private static final double MILES_TO_METERS = 1609.34;
 
     public DistanceAxisFormatter(Context context) {
         // Initialize the mem vars
         mContext = context;
-        mFormat = new DecimalFormat("###,###.##");
     }
 
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
 
         // Return the formatted String to be used as the axis labels
-        return mFormat.format(value) + MILES_LABEL;
+        return mContext.getString(R.string.list_guide_format_distance_imperial, value);
     }
 }
--- app/src/main/java/project/hikerguide/mpandroidchart/ElevationAxisFormatter.java
+++ app/src/main/java/project/hikerguide/mpandroidchart/ElevationAxisFormatter.java
@@ -7,30 +7,26 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.text.DecimalFormat;
 
+import project.hikerguide.R;
+
 
 /**
  * Created by Alvin on 7/25/2017.
  */
 
 public class ElevationAxisFormatter implements IAxisValueFormatter {
-    // ** Constants ** //
-    private static final String FEET_LABEL = " ft";
-    private static final String METERS_LABELS = " m";
-
     // ** Member Variables ** //
     Context mContext;
-    DecimalFormat mFormat;
 
     public ElevationAxisFormatter(Context context) {
         // Init the memvars
         mContext = context;
-        mFormat = new DecimalFormat("###,###");
     }
 
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
 
         // Return the formatted elevation
-        return mFormat.format(value) + FEET_LABEL;
+        return mContext.getString(R.string.list_guide_format_elevation_imperial, value);
     }
 }
