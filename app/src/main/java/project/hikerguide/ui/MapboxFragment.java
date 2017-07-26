364c19817a5fb8263a5e6c1d364970d94cb027b8
Alvin <amagh1990@gmail.com>
2017-07-25 19:26:05 -0700

    Add MapboxFragment for tracking MapView's lifecycle
    Same as the MapboxActivity, but for Fragments.

---
+++ app/src/main/java/project/hikerguide/ui/MapboxFragment.java
@@ -0,0 +1,84 @@
+package project.hikerguide.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+
+/**
+ * Created by Alvin on 7/25/2017.
+ */
+
+public class MapboxFragment extends Fragment {
+    // ** Member Variables ** //
+    MapView mMapView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (mMapView != null) {
+            mMapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mMapView != null) {
+            mMapView.onStart();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mMapView != null) {
+            mMapView.onPause();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mMapView != null) {
+            mMapView.onStop();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mMapView != null) {
+            mMapView.onSaveInstanceState(outState);
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mMapView != null) {
+            mMapView.onLowMemory();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mMapView != null) {
+            mMapView.onDestroy();
+        }
+    }
+
+    public void attachMapView(MapView mapView) {
+        mMapView = mapView;
+    }
+}
