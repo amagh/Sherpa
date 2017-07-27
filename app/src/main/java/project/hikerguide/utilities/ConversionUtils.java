8740456bfd8ed22202a7a66f7e2a3634fdc550e4
Alvin <amagh1990@gmail.com>
2017-07-27 14:48:41 -0700

    Modify conversion to use user preference for units

--- app/src/main/java/project/hikerguide/utilities/ConversionUtils.java
+++ app/src/main/java/project/hikerguide/utilities/ConversionUtils.java
@@ -16,13 +16,49 @@ public class ConversionUtils {
     private static final double METERS_PER_FEET = 0.3048;
     private static final double METERS_PER_KILOMETER = 1000;
 
+    /**
+     * Converts the distance to the correct units based on the user's preference
+     *
+     * @param context     Interface to global Context
+     * @param distance    Distance in meters
+     * @return Distance in either miles or kilometers based on user preference
+     */
     public static double convertDistance(Context context, double distance) {
 
-        return distance / METERS_PER_MILE;
+        // Get the user's unit preference
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String unitPref = prefs.getString(
+                context.getString(R.string.pref_units_key),
+                context.getString(R.string.pref_units_default));
+
+        // Convert units
+        if (unitPref.equals(context.getString(R.string.pref_units_imperial))) {
+            return distance / METERS_PER_MILE;
+        } else {
+            return distance / METERS_PER_KILOMETER;
+        }
     }
 
+    /**
+     * Converts the elevation to the correct units based on the user's preference
+     *
+     * @param context      Interface to global Context
+     * @param elevation    Elevation in meters
+     * @return Elevation in either feet or meters based on user preference
+     */
     public static double convertElevation(Context context, double elevation) {
 
-        return elevation / METERS_PER_FEET;
+        // Get the user's unit prefernce
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String unitPref = prefs.getString(
+                context.getString(R.string.pref_units_key),
+                context.getString(R.string.pref_units_default));
+
+        // Convert units
+        if (unitPref.equals(context.getString(R.string.pref_units_imperial))) {
+            return elevation / METERS_PER_FEET;
+        } else {
+            return elevation;
+        }
     }
 }
