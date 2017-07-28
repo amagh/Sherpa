41b575f7fe780b2e9dd571cd453cd5f893d06171
Alvin <amagh1990@gmail.com>
2017-07-27 19:55:33 -0700

    Add data binding to section with image
    Also loads images from local storage.

--- app/src/main/java/project/hikerguide/models/viewmodels/SectionViewModel.java
+++ app/src/main/java/project/hikerguide/models/viewmodels/SectionViewModel.java
@@ -3,15 +3,21 @@ package project.hikerguide.models.viewmodels;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import project.hikerguide.firebasestorage.StorageProvider;
 import project.hikerguide.models.datamodels.Section;
+
+import static project.hikerguide.utilities.StorageProviderUtils.IMAGE_PATH;
+import static project.hikerguide.utilities.StorageProviderUtils.JPEG_EXT;
 
 /**
  * Created by Alvin on 7/23/2017.
@@ -34,23 +40,53 @@ public class SectionViewModel extends BaseObservable {
     }
 
     @Bindable
+    public void setContent(String content) {
+        mSection.content = content;
+
+        notifyPropertyChanged(BR.content);
+    }
+
+    @Bindable
     public int getContentVisibility() {
         return mSection.content != null ? View.VISIBLE : View.GONE;
     }
 
     @Bindable
-    public StorageReference getImage() {
-        // Generate StorageReference for the image using the Section's FirebaseId
-        return mStorage.getReferenceForImage(mSection.firebaseId);
+    public Uri getImageUri() {
+        return mSection.getImageUri();
     }
 
-    @BindingAdapter("bind:image")
-    public static void loadImage(ImageView imageView, StorageReference image) {
-        // Use FirebaseUI's API for Glide to load the image using the StorageReference
-        Glide.with(imageView.getContext())
-                .using(new FirebaseImageLoader())
-                .load(image)
-                .into(imageView);
+    @Bindable
+    public StorageReference getImage() {
+
+        if (mSection.firebaseId == null) return null;
+
+        // Generate StorageReference for the image using the Section's FirebaseId
+        return FirebaseStorage.getInstance().getReference()
+                .child(IMAGE_PATH)
+                .child(mSection.firebaseId + JPEG_EXT);
+    }
+
+    @BindingAdapter({"bind:image", "bind:imageUri"})
+    public static void loadImage(ImageView imageView, StorageReference image, Uri imageUri) {
+
+        System.out.println(imageUri);
+
+        // Check whether to load image from File or from Firebase Storage
+        if (image == null) {
+
+            // No StorageReference, load local file using the File's Uri
+            Glide.with(imageView.getContext())
+                    .load(imageUri)
+                    .into(imageView);
+        } else {
+
+            // Load from Firebase Storage
+            Glide.with(imageView.getContext())
+                    .using(new FirebaseImageLoader())
+                    .load(image)
+                    .into(imageView);
+        }
     }
 
 }
