51b6e28757388fb43cc22cb57c68f75e224c9bd3
Alvin <amagh1990@gmail.com>
2017-07-31 15:49:21 -0700

    Add animation when changing UI in AccountActivity layout
    When switching from the default Sign-in page, the Create account layout
    includes an extra password field for confirming password creation.

--- app/src/main/java/project/hikerguide/models/viewmodels/AccountViewModel.java
+++ app/src/main/java/project/hikerguide/models/viewmodels/AccountViewModel.java
@@ -1,24 +1,43 @@
 package project.hikerguide.models.viewmodels;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.support.annotation.IntDef;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.android.databinding.library.baseAdapters.BR;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import at.wirecube.additiveanimations.additive_animator.AdditiveAnimator;
 import project.hikerguide.R;
+
+import static project.hikerguide.models.viewmodels.AccountViewModel.UiModes.CREATE_ACCOUNT;
+import static project.hikerguide.models.viewmodels.AccountViewModel.UiModes.SIGN_IN;
 
 /**
  * Created by Alvin on 7/28/2017.
  */
 
 public class AccountViewModel extends BaseObservable {
+    // ** Constants ** //
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({SIGN_IN, CREATE_ACCOUNT})
+    @interface UiModes {
+        int SIGN_IN = 0;
+        int CREATE_ACCOUNT = 1;
+    }
+
     // ** Member Variables ** //
     private Context mContext;
-    private boolean mCreateAccount;
+    @UiModes private int mUiMode;
 
     private String email;
     private String password;
@@ -29,68 +48,93 @@ public class AccountViewModel extends BaseObservable {
     }
 
     @Bindable
+    public @AccountViewModel.UiModes int getUiMode() {
+        return mUiMode;
+    }
+
+    @Bindable
     public String getPasswordHint() {
-        if (!mCreateAccount) {
-            return mContext.getString(R.string.sign_in_password_hint);
-        } else {
-            return mContext.getString(R.string.sign_in_password_confirm);
+        switch (mUiMode) {
+            case SIGN_IN: return mContext.getString(R.string.sign_in_password_hint);
+            case CREATE_ACCOUNT: return mContext.getString(R.string.sign_in_password_confirm);
         }
+
+        return null;
     }
 
     @Bindable
-    public String getSignInButtonText() {
-        if (!mCreateAccount) {
-            return mContext.getString(R.string.sign_in_button_text);
-        } else {
-            return mContext.getString(R.string.create_account_button_text);
+    public String getSwitchBtnText() {
+        switch(mUiMode) {
+            case SIGN_IN: return mContext.getString(R.string.create_account_button_text);
+            case CREATE_ACCOUNT: return mContext.getString(R.string.return_to_sign_in_button_text);
+        }
+
+        return null;
+    }
+
+    @BindingAdapter({"app:passwordTv", "app:confirmTv", "app:signInBtn", "app:createAccountBtn", "bind:switchBtnText", "bind:uiMode"})
+    public static void switchUi(Button switchUiButton, EditText passwordTv, EditText confirmTv,
+                                Button signInButton, Button createAccountButton, String switchBtnText, @UiModes int uiMode) {
+
+        // Default state variables
+        float confirmAlpha          = 0;
+        float signInAlpha           = 1;
+        float height                = 0;
+
+        // Modify variables if in CREATE_ACCOUNT mode
+        if (uiMode == CREATE_ACCOUNT) {
+            signInAlpha = 0;                                        // Hide sign in button
+            confirmAlpha = 1;                                       // Show password confirmation
+            ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) passwordTv.getLayoutParams();
+            height = params.topMargin + passwordTv.getHeight();     // Move password field down
+            confirmTv.setVisibility(View.VISIBLE);                  // Show confirm password field
+        }
+
+        // Animate state changes
+        new AdditiveAnimator().setDuration(150)
+                .target(switchUiButton).alpha(0)
+                .target(signInButton).alpha(signInAlpha)
+                .target(createAccountButton).alpha(confirmAlpha)
+                .target(passwordTv).translationY(height)
+                .target(confirmTv).alpha(confirmAlpha)
+                .start();
+
+        // Switch text on the button
+        switchUiButton.setText(switchBtnText);
+
+        // Re-show the switch UI button
+        AdditiveAnimator.animate(switchUiButton).alpha(1).start();
+
+        // If in sign-in mode, hide the confirm password EditText so that pressing "Next" on the
+        // email EditText, skips directly to the password EditText
+        if (uiMode == SIGN_IN) {
+            confirmTv.setVisibility(View.GONE);
         }
     }
 
-    @Bindable
-    public String getCreateAccountButtonText() {
-        if (!mCreateAccount) {
-            return mContext.getString(R.string.create_account_button_text);
-        } else {
-            return mContext.getString(R.string.return_to_sign_in_button_text);
+    /**
+     * Changes the UI to add a password confirmation EditText so that the user can create an
+     * account
+     *
+     * @param view    The Button being clicked
+     */
+    public void onClickSwitchUi(View view) {
+        switch (mUiMode) {
+            case SIGN_IN: mUiMode = CREATE_ACCOUNT;
+                break;
+            case CREATE_ACCOUNT: mUiMode = SIGN_IN;
+                break;
         }
-    }
 
-    @Bindable
-    public int getPasswordConfirmVisibility() {
-        if (!mCreateAccount) {
-            return View.GONE;
-        } else {
-            return View.VISIBLE;
-        }
-    }
-
-    @BindingAdapter("bind:passwordConfirmVisibility")
-    public static void animatePasswordConfirm(EditText editText, int visibility) {
-
-    }
-
-    public void onClickCreateAccount(View view) {
-        if (!mCreateAccount) {
-            mCreateAccount = true;
-            notifyPropertyChanged(BR._all);
-        } else {
-            mCreateAccount = false;
-            //  Return to sign in
-        }
+        notifyPropertyChanged(BR._all);
     }
 
     public void onClickSignIn(View view) {
-        if (!mCreateAccount) {
-            // Sign in
-        } else {
-            if (!password.equals(passwordConfirm)) {
-                // Passwords are different
-            } else if (password.length() < 8 || passwordConfirm.length() < 8) {
-                // Password does not meet min length
-            } else {
-                // Create Account
-            }
-        }
+        // Sign in
+    }
+
+    public void onClickCreateAccount(View view) {
+        // Create account
     }
 
     @Bindable
