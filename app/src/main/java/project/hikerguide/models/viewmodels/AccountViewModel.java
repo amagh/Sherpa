cd0c9e62104a599a5b79c3550c57ffb2942617f2
Alvin <amagh1990@gmail.com>
2017-07-31 18:25:36 -0700

    Add methods for handling account creation/sign in
    Uses FirebaseAuth to handle account creation and sign in.

--- app/src/main/java/project/hikerguide/models/viewmodels/AccountViewModel.java
+++ app/src/main/java/project/hikerguide/models/viewmodels/AccountViewModel.java
@@ -1,17 +1,26 @@
 package project.hikerguide.models.viewmodels;
 
-import android.animation.ValueAnimator;
+import android.app.Activity;
 import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
 import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.databinding.library.baseAdapters.BR;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -35,6 +44,8 @@ public class AccountViewModel extends BaseObservable {
         int CREATE_ACCOUNT = 1;
     }
 
+    private static final String emailValidationRegex = "^(?:(?:[\\w`~!#$%^&*\\-=+;:{}'|,?\\/]+(?:(?:\\.(?:\"(?:\\\\?[\\w`~!#$%^&*\\-=+;:{}'|,?\\/\\.()<>\\[\\] @]|\\\\\"|\\\\\\\\)*\"|[\\w`~!#$%^&*\\-=+;:{}'|,?\\/]+))*\\.[\\w`~!#$%^&*\\-=+;:{}'|,?\\/]+)?)|(?:\"(?:\\\\?[\\w`~!#$%^&*\\-=+;:{}'|,?\\/\\.()<>\\[\\] @]|\\\\\"|\\\\\\\\)+\"))@(?:[a-zA-Z\\d\\-]+(?:\\.[a-zA-Z\\d\\-]+)*|\\[\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\])$";
+
     // ** Member Variables ** //
     private Context mContext;
     @UiModes private int mUiMode;
@@ -42,6 +53,7 @@ public class AccountViewModel extends BaseObservable {
     private String email;
     private String password;
     private String passwordConfirm;
+    private int progressVisibility = View.GONE;
 
     public AccountViewModel(Context context) {
         mContext = context;
@@ -72,8 +84,13 @@ public class AccountViewModel extends BaseObservable {
         return null;
     }
 
+    @Bindable
+    public int getProgressVisibility() {
+        return progressVisibility;
+    }
+
     @BindingAdapter({"app:passwordTv", "app:confirmTv", "app:signInBtn", "app:createAccountBtn", "bind:switchBtnText", "bind:uiMode"})
-    public static void switchUi(Button switchUiButton, EditText passwordTv, EditText confirmTv,
+    public static void switchUi(Button switchUiButton, final EditText passwordTv, final EditText confirmTv,
                                 Button signInButton, Button createAccountButton, String switchBtnText, @UiModes int uiMode) {
 
         // Default state variables
@@ -81,34 +98,49 @@ public class AccountViewModel extends BaseObservable {
         float signInAlpha           = 1;
         float height                = 0;
 
-        // Modify variables if in CREATE_ACCOUNT mode
-        if (uiMode == CREATE_ACCOUNT) {
-            signInAlpha = 0;                                        // Hide sign in button
-            confirmAlpha = 1;                                       // Show password confirmation
-            ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) passwordTv.getLayoutParams();
-            height = params.topMargin + passwordTv.getHeight();     // Move password field down
-            confirmTv.setVisibility(View.VISIBLE);                  // Show confirm password field
-        }
+        if (confirmTv.getHeight() != 0) {
+            // Modify variables if in CREATE_ACCOUNT mode
+            if (uiMode == CREATE_ACCOUNT) {
+                signInAlpha = 0;                                        // Hide sign in button
+                confirmAlpha = 1;                                       // Show password confirmation
+                confirmTv.setVisibility(View.VISIBLE);                  // Show confirm password field
+                createAccountButton.setVisibility(View.VISIBLE);        // Allow click create acct button
+                signInButton.setVisibility(View.GONE);                  // Prevent click sign in button
+            } else {
+                ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) confirmTv.getLayoutParams();
+                height = -(params.topMargin + passwordTv.getHeight());  // Move password field up
+                signInButton.setVisibility(View.VISIBLE);               // Allow click sign in button
+            }
 
-        // Animate state changes
-        new AdditiveAnimator().setDuration(150)
-                .target(switchUiButton).alpha(0)
-                .target(signInButton).alpha(signInAlpha)
-                .target(createAccountButton).alpha(confirmAlpha)
-                .target(passwordTv).translationY(height)
-                .target(confirmTv).alpha(confirmAlpha)
-                .start();
+            // Animate state changes
+            new AdditiveAnimator().setDuration(150)
+                    .target(switchUiButton).alpha(0)
+                    .target(signInButton).alpha(signInAlpha)
+                    .target(createAccountButton).alpha(confirmAlpha)
+                    .target(passwordTv).translationY(height)
+                    .target(confirmTv).alpha(confirmAlpha)
+                    .start();
 
-        // Switch text on the button
-        switchUiButton.setText(switchBtnText);
+            // Switch text on the button
+            switchUiButton.setText(switchBtnText);
 
-        // Re-show the switch UI button
-        AdditiveAnimator.animate(switchUiButton).alpha(1).start();
+            // Re-show the switch UI button
+            AdditiveAnimator.animate(switchUiButton).alpha(1).start();
 
-        // If in sign-in mode, hide the confirm password EditText so that pressing "Next" on the
-        // email EditText, skips directly to the password EditText
-        if (uiMode == SIGN_IN) {
-            confirmTv.setVisibility(View.GONE);
+            // If in sign-in mode, hide the confirm password EditText so that pressing "Next" on the
+            // email EditText, skips directly to the password EditText
+            if (uiMode == SIGN_IN) {
+                confirmTv.setVisibility(View.GONE);
+                createAccountButton.setVisibility(View.GONE);
+            }
+        } else {
+            confirmTv.post(new Runnable() {
+                @Override
+                public void run() {
+                    passwordTv.setY(confirmTv.getY());
+                    confirmTv.setVisibility(View.GONE);
+                }
+            });
         }
     }
 
@@ -119,6 +151,7 @@ public class AccountViewModel extends BaseObservable {
      * @param view    The Button being clicked
      */
     public void onClickSwitchUi(View view) {
+        // Switch the UI Mode
         switch (mUiMode) {
             case SIGN_IN: mUiMode = CREATE_ACCOUNT;
                 break;
@@ -129,12 +162,111 @@ public class AccountViewModel extends BaseObservable {
         notifyPropertyChanged(BR._all);
     }
 
+    /**
+     * Signs a user in using FirebaseAuth
+     *
+     * @param view    View that was clicked
+     */
     public void onClickSignIn(View view) {
-        // Sign in
+        // Check that the email and password are valid
+        if (email == null || !email.matches(emailValidationRegex)) {
+            Toast.makeText(mContext, mContext.getString(R.string.invalid_email_error), Toast.LENGTH_LONG).show();
+            return;
+        } else if (password == null) {
+            Toast.makeText(mContext, mContext.getString(R.string.no_password_error), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Make ProgressBar visible
+        progressVisibility = View.VISIBLE;
+        notifyPropertyChanged(BR.progressVisibility);
+
+        // Attempt to sign in with credentials
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // Hide ProgressBar
+                        progressVisibility = View.GONE;
+                        notifyPropertyChanged(BR.progressVisibility);
+
+                        if (task.isSuccessful()) {
+                            ((Activity) mContext).finish();
+                        } else {
+                            Toast.makeText(mContext, mContext.getString(R.string.authentication_error), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
+    /**
+     * Creates a new account using FirebaseAuth
+     *
+     * @param view    View that was clicked
+     */
     public void onClickCreateAccount(View view) {
-        // Create account
+
+        // Check that email and password are valid
+        if (email == null || !email.matches(emailValidationRegex)) {
+            Toast.makeText(mContext, mContext.getString(R.string.invalid_email_error), Toast.LENGTH_LONG).show();
+        } else if (password == null || passwordConfirm == null) {
+            Toast.makeText(mContext, mContext.getString(R.string.no_password_error), Toast.LENGTH_LONG).show();
+        } else if (!password.equals(passwordConfirm)) {
+            Toast.makeText(mContext, mContext.getString(R.string.no_password_match), Toast.LENGTH_LONG).show();
+        } else if (password.length() < 8) {
+            Toast.makeText(mContext, mContext.getString(R.string.invalid_password_length_error), Toast.LENGTH_LONG).show();
+        } else {
+
+            // Make ProgressBar visible
+            progressVisibility = View.VISIBLE;
+            notifyPropertyChanged(BR.progressVisibility);
+
+            // Attempt to create a new account
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            // Hide ProgressBar
+                            progressVisibility = View.GONE;
+                            notifyPropertyChanged(BR.progressVisibility);
+
+                            if (task.isSuccessful()) {
+                                ((Activity) mContext).finish();
+                            }
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Listener for EditorAction on password EditText
+     *
+     * @param textView    The password EditText
+     * @param actionId    The IME action that was performed
+     * @param event       The event triggering the IME action
+     * @return True if EditorAction was handled. False otherwise
+     */
+    public boolean onPasswordEditorAction(TextView textView, int actionId, KeyEvent event) {
+
+        // Check that the ActionId is IME_ACTION_DONE
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+            // Sign in / create an account depending on the UI Mode
+            switch(mUiMode) {
+                case SIGN_IN:
+                    onClickSignIn(textView);
+                    break;
+                case CREATE_ACCOUNT:
+                    onClickCreateAccount(textView);
+                    break;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     @Bindable
