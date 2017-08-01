923c7e9083fb32055227962ff572c7de21b494ec
Alvin <amagh1990@gmail.com>
2017-08-01 11:37:09 -0700

    Add layout for view author details
    Used when user is checking an author's profile or their own profile.

--- app/src/main/java/project/hikerguide/models/viewmodels/AuthorViewModel.java
+++ app/src/main/java/project/hikerguide/models/viewmodels/AuthorViewModel.java
@@ -4,15 +4,16 @@ import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import project.hikerguide.R;
-import project.hikerguide.firebasestorage.StorageProvider;
 import project.hikerguide.models.datamodels.Author;
 
 import static project.hikerguide.utilities.StorageProviderUtils.IMAGE_PATH;
@@ -26,6 +27,7 @@ public class AuthorViewModel extends BaseObservable {
     // ** Member Variables ** //
     private Author mAuthor;
     private Context mContext;
+    private int mEditVisibility = View.INVISIBLE;
 
     public AuthorViewModel(Context context, Author author) {
         mAuthor = author;
@@ -55,5 +57,45 @@ public class AuthorViewModel extends BaseObservable {
     @Bindable
     public String getScore() {
         return mContext.getString(R.string.list_author_format_score, mAuthor.score);
+    }
+
+    @Bindable
+    public String getDescription() {
+        return mAuthor.description;
+    }
+
+    @Bindable
+    public int getEditVisibility() {
+        return mEditVisibility;
+    }
+
+    @BindingAdapter("bind:editVisibility")
+    public static void setEditVisibility(ImageView imageView, int editVisibility) {
+
+        // Enable/disable click on ImageView depending on whether use is viewing their own profile.
+        // The edit button is only visible when user is viewing their own profile.
+        if (editVisibility == View.INVISIBLE) {
+            imageView.setClickable(false);
+        } else {
+            imageView.setClickable(true);
+        }
+
+        // Set visibility of the edit button
+        imageView.setVisibility(editVisibility);
+    }
+
+    public void onClickEdit(View view) {
+
+    }
+
+    /**
+     * Enables editing of the information in the author's profile
+     */
+    public void enableEditing() {
+
+        // Set the edit button to visible
+        mEditVisibility = View.VISIBLE;
+
+        notifyPropertyChanged(BR.editVisibility);
     }
 }
