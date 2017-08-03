2569fa03073156e8d7e2b650ceb7cf7c813bd679
Alvin <amagh1990@gmail.com>
2017-08-03 12:15:48 -0700

    Update layout for TrailActivity
    Now sets the Adapter and LayoutManager for the RecyclerView. Animation
    for search bar now properly animates change in focus. Querying now
    sort of works, but queries all trails instead of only trails in the
    same area as the one the user selected.

--- app/src/main/java/project/hikerguide/models/viewmodels/SearchTrailViewModel.java
+++ app/src/main/java/project/hikerguide/models/viewmodels/SearchTrailViewModel.java
@@ -3,6 +3,28 @@ package project.hikerguide.models.viewmodels;
 import android.app.Activity;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+
+import at.wirecube.additiveanimations.additive_animator.AdditiveAnimator;
+import project.hikerguide.BR;
+import project.hikerguide.data.GuideDatabase;
+import project.hikerguide.firebasedatabase.DatabaseProvider;
+import project.hikerguide.models.datamodels.Trail;
+import project.hikerguide.ui.adapters.TrailAdapter;
+import project.hikerguide.utilities.FirebaseProviderUtils;
 
 /**
  * Created by Alvin on 8/3/2017.
@@ -11,6 +33,7 @@ import android.databinding.Bindable;
 public class SearchTrailViewModel extends BaseObservable {
     // ** Member Variables ** //
     private Activity mActivity;
+    private TrailAdapter mAdapter;
     private String mQuery;
     private boolean mSearchHasFocus = false;
 
@@ -25,5 +48,123 @@ public class SearchTrailViewModel extends BaseObservable {
 
     public void setQuery(String query) {
         mQuery = query;
+
+        if (mQuery.length() > 2) {
+            queryFirebase(query);
+        } else {
+            mAdapter.setTrailList(null);
+        }
+    }
+
+    @Bindable
+    public Activity getActivity() {
+        return mActivity;
+    }
+
+    @Bindable
+    public TrailAdapter getAdapter() {
+        if (mAdapter == null) {
+            mAdapter = new TrailAdapter();
+        }
+
+        return mAdapter;
+    }
+
+    @BindingAdapter({"bind:activity", "bind:adapter"})
+    public static void initRecyclerView(RecyclerView recyclerView, Activity activity, TrailAdapter adapter) {
+
+        // Setup the RecyclerView
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity));
+    }
+
+    @Bindable
+    public boolean getHasFocus() {
+        return mSearchHasFocus;
+    }
+
+    @BindingAdapter({"app:searchIv", "app:closeIv", "bind:hasFocus"})
+    public static void animateFocus(EditText searchTv, ImageView searchIv, ImageView closeIv, boolean hasFocus) {
+
+        float searchAlpha   = 0;
+        float closeAlpha    = 0;
+
+        if (hasFocus) {
+            // Prevent clicking on the close ImageView when it is not visible
+            closeIv.setClickable(true);
+            closeAlpha = 1;
+        } else {
+            searchAlpha = 1;
+
+            // Allow clicking on the close ImageView
+            closeIv.setClickable(false);
+
+            // Clear the Focus from the EditText
+            searchTv.clearFocus();
+        }
+
+        // Animate changes
+        new AdditiveAnimator().setDuration(150)
+                .target(searchIv).alpha(searchAlpha)
+                .target(closeIv).alpha(closeAlpha)
+                .start();
+    }
+
+    private void queryFirebase(String query) {
+
+        // Convert the query to lowercase
+        query = query.toLowerCase();
+
+        // Query the Firebase Database
+        final Query trailQuery = FirebaseDatabase.getInstance().getReference()
+                .child(GuideDatabase.TRAILS)
+                .orderByChild("lowerCaseName")
+                .startAt(query)
+                .endAt(query + "z");
+
+        trailQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // Check that the result is valid
+                if (dataSnapshot.exists()) {
+
+                    // Retrieve the Trails from the DataSnapshot
+                    Trail[] trails = (Trail[]) FirebaseProviderUtils.getModelsFromSnapshot(DatabaseProvider.FirebaseType.TRAIL, dataSnapshot);
+
+                    // Pass the Trails to the Adapter
+                    mAdapter.setTrailList(Arrays.asList(trails));
+                }
+
+                // Remove Listener
+                trailQuery.removeEventListener(this);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                // Remove Listener
+                trailQuery.removeEventListener(this);
+            }
+        });
+    }
+
+    public void onFocusChanged(View view, boolean hasFocus) {
+        mSearchHasFocus = hasFocus;
+
+        notifyPropertyChanged(BR.hasFocus);
+    }
+
+
+    public void onClickClear(View view) {
+
+        // Set the focus to false
+        mSearchHasFocus = false;
+
+        // Clear the query
+        mQuery = null;
+
+        notifyPropertyChanged(BR.query);
+        notifyPropertyChanged(BR.hasFocus);
     }
 }
