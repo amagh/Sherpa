f8086553692f5ce54a7aec6489341a5acb8ad69a
Alvin <amagh1990@gmail.com>
2017-08-04 17:23:07 -0700

    Implement uploading of completed Guides
    PublishActivity now uploads Files associated with Guides and the data
    for the Guide to Firebase.

--- app/src/main/java/project/hikerguide/ui/activities/PublishActivity.java
+++ app/src/main/java/project/hikerguide/ui/activities/PublishActivity.java
@@ -3,14 +3,32 @@ package project.hikerguide.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
 
-import java.io.File;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import project.hikerguide.files.abstractfiles.BaseFile;
 import project.hikerguide.models.datamodels.Area;
 import project.hikerguide.models.datamodels.Author;
 import project.hikerguide.models.datamodels.Guide;
 import project.hikerguide.models.datamodels.Section;
 import project.hikerguide.models.datamodels.Trail;
+import project.hikerguide.models.datamodels.abstractmodels.BaseModel;
+import project.hikerguide.utilities.FirebaseProviderUtils;
 import project.hikerguide.utilities.SaveUtils;
 import timber.log.Timber;
 
@@ -19,6 +37,7 @@ import static project.hikerguide.utilities.IntentKeys.AUTHOR_KEY;
 import static project.hikerguide.utilities.IntentKeys.GUIDE_KEY;
 import static project.hikerguide.utilities.IntentKeys.SECTION_KEY;
 import static project.hikerguide.utilities.IntentKeys.TRAIL_KEY;
+import static project.hikerguide.utilities.StorageProviderUtils.getReferenceForFile;
 
 /**
  * Created by Alvin on 8/3/2017.
@@ -31,6 +50,9 @@ public class PublishActivity extends MapboxActivity {
     private Area mArea;
     private Trail mTrail;
     private Section[] mSections;
+
+    private List<StorageReference> mUploadReferenceList;
+    private UploadListener mUploadListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +75,8 @@ public class PublishActivity extends MapboxActivity {
 
         // Resize any images associated with the models
         resizeImages();
+
+        mUploadListener = new UploadListener(getChildUpdates());
     }
 
     /**
@@ -68,6 +92,207 @@ public class PublishActivity extends MapboxActivity {
             if (section.hasImage) {
                 SaveUtils.resizeImageForModel(section);
             }
+        }
+    }
+
+    /**
+     * Creates the Map that will be used to push the data for the models to Firebase Database
+     *
+     * @return The Map including all the data for the models that will be pushed to Firebase
+     * Database
+     */
+    private Map<String, Object> getChildUpdates() {
+
+        // Initialize the Map
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        // Upload the Area if it does not exist in the Firebase Database
+        if (mArea.firebaseId == null) {
+            addChildUpdate(mArea, childUpdates);
+        }
+
+        // Upload the trail
+        if (mTrail.firebaseId == null) {
+            mTrail.areaId = mArea.firebaseId;
+            addChildUpdate(mTrail, childUpdates);
+        }
+
+        // Upload the Guide
+        if (mGuide.firebaseId == null) {
+
+            // Set the variables of the Guide to those from the associated Area and trail
+            mGuide.trailId = mTrail.firebaseId;
+            mGuide.trailName = mTrail.name;
+            mGuide.authorId = mAuthor.firebaseId;
+            mGuide.authorName = mAuthor.name;
+            mGuide.area = mArea.name;
+
+            addChildUpdate(mGuide, childUpdates);
+
+            // Upload the Files for the Guide
+            uploadFile(mGuide.getGpxFile());
+            uploadFile(mGuide.getImageFile());
+        }
+
+        // Upload each Section
+        for (Section section : mSections) {
+            section.guideId = mGuide.firebaseId;
+
+            addChildUpdate(section, childUpdates);
+
+            if (section.hasImage) {
+
+                // If the Section has an image, upload it
+                uploadFile(section.getImageFile());
+            }
+        }
+
+        return childUpdates;
+    }
+
+    /**
+     * Adds a data models' data to the Map of updates to push to the FirebaseDatabase
+     *
+     * @param model           The model to be updated
+     * @param childUpdates    The Map containing all update operations
+     */
+    private void addChildUpdate(BaseModel model, Map<String, Object> childUpdates) {
+
+        // Get the Firebase Database directory specific to the model
+        String directory = FirebaseProviderUtils.getDirectoryFromModel(model);
+
+        // Push an update to get the key for the update
+        String firebaseId;
+        if (model instanceof Section) {
+            firebaseId = FirebaseDatabase.getInstance().getReference()
+                    .child(directory)
+                    .child(((Section) model).guideId)
+                    .push()
+                    .getKey();
+
+            directory = FirebaseProviderUtils.getDirectoryFromModel(model) + "/" + ((Section) model).guideId;
+        } else {
+            firebaseId = FirebaseDatabase.getInstance().getReference()
+                    .child(directory)
+                    .push()
+                    .getKey();
+        }
+
+        // Modify the directory to include the FirebaseId (key)
+        directory = directory + "/" + firebaseId;
+
+        // Add the update to the Map
+        childUpdates.put(directory, model.toMap());
+
+        // Set the Model's FirebaseId to the upload key
+        model.firebaseId = firebaseId;
+    }
+
+    /**
+     * Uploads a File to FirebaseStorage
+     *
+     * @param file    The File to be uploaded
+     */
+    private void uploadFile(BaseFile file) {
+
+        // Get a reference to Firebase Storage
+        StorageReference reference = FirebaseStorage.getInstance().getReference();
+
+        // Get the File-specific Storage Reference
+        reference = getReferenceForFile(reference, file);
+
+        // Add the Reference to the list of uploads to track
+        addUploadReference(reference);
+
+        try {
+
+            // Create a FIS from the File
+            FileInputStream inStream = new FileInputStream(file);
+
+            // Upload
+            reference.putStream(inStream)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot snapshot) {
+
+                            // Remove the task from the tracked List
+                            onUploadComplete(snapshot.getStorage());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            // Inform the user that it did not complete and will allow them
+                            // to try again
+                            Toast.makeText(PublishActivity.this, "Upload failed! Please try again.", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Adds an upload task to track for completion to mUploadReferenceList
+     *
+     * @param uploadReference    The StorageReference for the task to be tracked
+     */
+    private void addUploadReference(StorageReference uploadReference) {
+
+        // Init the List if necessary
+        if (mUploadReferenceList == null) {
+            mUploadReferenceList = new ArrayList<>();
+        }
+
+        // Add the StorageReference to the List
+        mUploadReferenceList.add(uploadReference);
+
+        Timber.d("Uploads remaining: " + mUploadReferenceList.size());
+    }
+
+    /**
+     * Removes the StorageReference from mUploadReferenceList that was completed
+     *
+     * @param uploadReference    The StorageReference that was completed
+     */
+    private void onUploadComplete(StorageReference uploadReference) {
+
+        // Iterate through each the StorageReferences to find the matching StorageReference
+        for (StorageReference reference : mUploadReferenceList) {
+            if (reference.equals(uploadReference)) {
+
+                // Remove the Reference from the List
+                mUploadReferenceList.remove(mUploadReferenceList.indexOf(reference));
+                break;
+            }
+        }
+
+        if (mUploadReferenceList.size() == 0) {
+
+            // If all upload tasks have completed, update the Firebase Database
+            mUploadListener.onUploadComplete();
+        }
+
+        Timber.d("Removing task: " + mUploadReferenceList.size() + " remaining");
+    }
+
+    private class UploadListener {
+        // ** Member Variables ** //
+        private Map<String, Object> mChildUpdates;
+
+        private UploadListener(Map<String, Object> childUpdates) {
+            mChildUpdates = childUpdates;
+        }
+
+        /**
+         * Updates the Firebase Database with the data loaded into mChildUpdates
+         */
+        private void onUploadComplete() {
+            FirebaseDatabase.getInstance().getReference()
+                    .updateChildren(mChildUpdates);
         }
     }
 }
