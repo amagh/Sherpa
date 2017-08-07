9f17a4af5f068e9441cc45059d8926e2cf6429df
Alvin <amagh1990@gmail.com>
2017-08-07 15:49:44 -0700

    Add method in GuideDetailsActivity for switching Fragments

--- app/src/main/java/project/hikerguide/ui/activities/GuideDetailsActivity.java
+++ app/src/main/java/project/hikerguide/ui/activities/GuideDetailsActivity.java
@@ -1,40 +1,20 @@
 package project.hikerguide.ui.activities;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.view.menu.MenuAdapter;
-import android.support.v7.widget.LinearLayoutManager;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import project.hikerguide.R;
-import project.hikerguide.data.GuideDatabase;
 import project.hikerguide.databinding.ActivityGuideDetailsBinding;
-import project.hikerguide.firebasedatabase.DatabaseProvider;
-import project.hikerguide.models.datamodels.Author;
 import project.hikerguide.models.datamodels.Guide;
-import project.hikerguide.models.datamodels.Section;
-import project.hikerguide.models.viewmodels.GuideViewModel;
-import project.hikerguide.ui.adapters.GuideDetailsAdapter;
 import project.hikerguide.ui.adapters.GuideDetailsFragmentAdapter;
 import project.hikerguide.ui.fragments.GuideDetailsFragment;
 import project.hikerguide.ui.fragments.GuideDetailsMapFragment;
-import project.hikerguide.utilities.FirebaseProviderUtils;
 import timber.log.Timber;
 
-import static project.hikerguide.utilities.IntentKeys.AUTHOR_KEY;
 import static project.hikerguide.utilities.IntentKeys.GUIDE_KEY;
 
 public class GuideDetailsActivity extends MapboxActivity {
@@ -48,22 +28,43 @@ public class GuideDetailsActivity extends MapboxActivity {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_guide_details);
 
+        // Get the Guide passed in the Intent
         if (getIntent().getParcelableExtra(GUIDE_KEY) != null) {
             mGuide = getIntent().getParcelableExtra(GUIDE_KEY);
         } else {
             Timber.d("No guide passed in Intent!");
         }
 
+        // Init the ViewPager
         initViewPager();
     }
 
+    /**
+     * Initializes the variables required for the ViewPager
+     */
     private void initViewPager() {
+
+        // Initialize the Adapter
         mAdapter = new GuideDetailsFragmentAdapter(getSupportFragmentManager());
 
+        // Initialize the Fragments that will be contained by the ViewPager
         List<Fragment> fragmentList = new ArrayList<>();
         fragmentList.add(GuideDetailsFragment.newInstance(mGuide));
+        fragmentList.add(GuideDetailsMapFragment.newInstance(mGuide));
 
+        // Set the List of Fragments for the Adapter
         mAdapter.swapFragmentList(fragmentList);
+
+        // Set the Adapter
         mBinding.guideDetailsVp.setAdapter(mAdapter);
+    }
+
+    /**
+     * Changes the page currently displayed in the ViewPager
+     *
+     * @param page    The page to switch to
+     */
+    public void switchPage(int page) {
+        mBinding.guideDetailsVp.setCurrentItem(page);
     }
 }
