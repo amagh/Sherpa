7b9d0715b8b257909542dfde0f1ee3b01b34823c
Alvin <amagh1990@gmail.com>
2017-08-21 13:25:06 -0700

    Update Ratings data model to include author info
    This will allow the author image and name to be populated without
    making a separate query to the database for the information.

--- app/src/main/java/project/hikerguide/models/datamodels/Rating.java
+++ app/src/main/java/project/hikerguide/models/datamodels/Rating.java
@@ -2,7 +2,9 @@ package project.hikerguide.models.datamodels;
 
 import com.google.firebase.database.Exclude;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,16 +15,23 @@ public class Rating {
     // ** Constants ** //
     private static final String COMMENT     = "comment";
     private static final String RATING      = "rating";
+    private static final String AUTHOR_NAME = "authorName";
 
     // ** Member Variables ** //
     private String comment;
     private int rating;
+    private String authorId;
+    private String authorName;
 
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
 
         map.put(COMMENT, comment);
         map.put(RATING, rating);
+
+        if (authorName != null) {
+            map.put(AUTHOR_NAME, authorName);
+        }
 
         return map;
     }
@@ -40,6 +49,7 @@ public class Rating {
         return false;
     }
 
+
     //********************************************************************************************//
     //*********************************** Getters and Setters ************************************//
     //********************************************************************************************//
@@ -52,11 +62,27 @@ public class Rating {
         return rating;
     }
 
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
     public void setComment(String comment) {
         this.comment = comment;
     }
 
     public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
     }
 }
