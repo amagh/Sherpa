eb6477169062eff13ba7b44bd334e65f9a9e1fb6
Alvin <amagh1990@gmail.com>
2017-08-22 14:23:47 -0700

    Update GuideDetailsAdapter to handle Ratings
    Now it enabled a logged in user to add a review for a Guide and
    displays all reviews for a Guide at the end of the Guide.

--- app/src/main/java/project/hikerguide/ui/adapters/GuideDetailsAdapter.java
+++ app/src/main/java/project/hikerguide/ui/adapters/GuideDetailsAdapter.java
@@ -8,19 +8,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import project.hikerguide.R;
+import project.hikerguide.databinding.ListItemAddRatingBinding;
 import project.hikerguide.databinding.ListItemAuthorBinding;
 import project.hikerguide.databinding.ListItemGuideDetailsBinding;
+import project.hikerguide.databinding.ListItemRatingBinding;
 import project.hikerguide.databinding.ListItemSectionImageBinding;
 import project.hikerguide.databinding.ListItemSectionTextBinding;
 import project.hikerguide.models.datamodels.Author;
 import project.hikerguide.models.datamodels.Guide;
+import project.hikerguide.models.datamodels.Rating;
 import project.hikerguide.models.datamodels.Section;
 import project.hikerguide.models.datamodels.abstractmodels.BaseModel;
 import project.hikerguide.models.viewmodels.AuthorViewModel;
 import project.hikerguide.models.viewmodels.GuideViewModel;
+import project.hikerguide.models.viewmodels.RatingViewModel;
 import project.hikerguide.models.viewmodels.SectionViewModel;
 import project.hikerguide.ui.activities.MapboxActivity;
+import project.hikerguide.utilities.FirebaseProviderUtils;
+import timber.log.Timber;
 
 /**
  * Created by Alvin on 7/22/2017.
@@ -28,17 +36,21 @@ import project.hikerguide.ui.activities.MapboxActivity;
 
 public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapter.GuideDetailsViewHolder>{
     // ** Constants ** //
-    private static final int GUIDE_VIEW_TYPE = 0;
-    private static final int SECTION_VIEW_TYPE = 1;
-    private static final int SECTION_IMAGE_VIEW_TYPE = 2;
-    private static final int AUTHOR_VIEW_TYPE = 3;
+    private static final int GUIDE_VIEW_TYPE            = 0;
+    private static final int SECTION_VIEW_TYPE          = 1;
+    private static final int SECTION_IMAGE_VIEW_TYPE    = 2;
+    private static final int AUTHOR_VIEW_TYPE           = 3;
+    private static final int RATING_VIEW_TYPE           = 4;
+    private static final int ADD_RATING_VIEW_TYPE       = 5;
 
     // ** Member Variables ** //
     private Guide mGuide;
     private Section[] mSections;
     private Author mAuthor;
+    private Rating[] mRatings;
+    private Author mUser;
 
-    private BaseModel[] mModels;
+    private Object[] mModels;
 
     private MapboxActivity mActivity;
 
@@ -46,6 +58,19 @@ public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapte
 
     public GuideDetailsAdapter(ClickHandler clickHandler) {
         mClickHandler = clickHandler;
+
+        // If User is logged in, get the Author data model that represents them
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+            FirebaseProviderUtils.getAuthorForFirebaseUser(new FirebaseProviderUtils.FirebaseListener() {
+                @Override
+                public void onModelReady(BaseModel model) {
+
+                    mUser = (Author) model;
+                    updateModels();
+                }
+            });
+        }
     }
 
     @Override
@@ -71,10 +96,19 @@ public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapte
                  layoutId = R.layout.list_item_author;
                  break;
 
+             case RATING_VIEW_TYPE:
+                 layoutId = R.layout.list_item_rating;
+                 break;
+
+             case ADD_RATING_VIEW_TYPE:
+                 layoutId = R.layout.list_item_add_rating;
+                 break;
+
              default: throw new UnsupportedOperationException("Unknown view type: " + viewType);
          }
 
-        return new GuideDetailsViewHolder(DataBindingUtil.inflate(inflater, layoutId, parent, false));
+         ViewDataBinding binding = DataBindingUtil.inflate(inflater, layoutId, parent, false);
+        return new GuideDetailsViewHolder(binding, this);
     }
 
     @Override
@@ -96,19 +130,30 @@ public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapte
     @Override
     public int getItemViewType(int position) {
 
-        if (position == 0) {
-            // The first item is reserved for the guide details
+        Object model = mModels[position];
+
+        // Return the ViewType based on the type of model in that position
+        if (model instanceof Guide) {
             return GUIDE_VIEW_TYPE;
-        } else if (position == mModels.length - 1) {
-            // The last item is for the author's details
+        } else if (model instanceof Author) {
             return AUTHOR_VIEW_TYPE;
-        } else if (!((Section) mModels[position]).hasImage){
-            // If the Section does not have an image, inflate the layout without an image
-            return SECTION_VIEW_TYPE;
-        } else {
-            // Otherwise, the Section does have an image and requires the alternate layout
-            return SECTION_IMAGE_VIEW_TYPE;
+        } else if (model instanceof Section) {
+            if (((Section) model).hasImage) {
+                return SECTION_IMAGE_VIEW_TYPE;
+            } else {
+                return SECTION_VIEW_TYPE;
+            }
+        } else if (model instanceof Rating) {
+
+            // For adding a new rating or editing a user's previous rating
+            if (((Rating) model).getAuthorId() == null) {
+                return ADD_RATING_VIEW_TYPE;
+            } else {
+                return RATING_VIEW_TYPE;
+            }
         }
+
+        return super.getItemViewType(position);
     }
 
     /**
@@ -152,23 +197,86 @@ public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapte
     private void updateModels() {
 
         // Check to ensure all required items are present
-        if (mGuide != null && mSections != null && mAuthor != null) {
-
-            // Create a new Array 2 larger than the length of mSections to leave room for the Guide
-            // details and Author
-            mModels = new BaseModel[mSections.length + 2];
-
-            // First item is the Guide's details
-            mModels[0] = mGuide;
-
-            // Copy the mSections Array into the middle sections of mModels
-            System.arraycopy(mSections, 0, mModels, 1, mSections.length);
-
-            // Set the last item to the Author
-            mModels[mModels.length - 1] = mAuthor;
-
-            notifyDataSetChanged();
+        if (mGuide == null || mSections == null || mAuthor == null) {
+            return;
         }
+
+        if (mGuide.raters != null && mGuide.raters.size() > 0) {
+            mRatings = mGuide.getRatings();
+        }
+
+        // Check if there are Ratings to be added to the Adapter
+        if (mUser != null && !mGuide.authorId.equals(mUser.firebaseId)) {
+
+            if (mGuide.raters != null && mGuide.raters.containsKey(mUser.firebaseId)) {
+
+                mModels = new Object[mSections.length + mRatings.length + 2];
+
+                // Iterate through the Ratings and find the User's rating and place it at the top
+                for (int i = 0; i < mRatings.length; i++) {
+                    Rating rating = mRatings[i];
+
+                    if (rating.getAuthorId().equals(mUser.firebaseId)) {
+
+                        // Set the first review to be viewed to the user's own ratin
+                        mModels[mSections.length + 2] = rating;
+
+                        // Copy the rest of the ratings after the user's rating
+                        if (i != 0) {
+                            System.arraycopy(mRatings, 0, mModels, mSections.length + 3, i);
+                        }
+
+                        if (i < mRatings.length - 1) {
+                            System.arraycopy(mRatings, i + 1, mModels, mSections.length + 3, mRatings.length - i - 1);
+                        }
+
+                        break;
+                    }
+                }
+
+            } else {
+                if (mRatings == null) {
+                    // Create a new Array for mRatings
+                    mRatings = new Rating[0];
+                }
+
+                mModels = new Object[mSections.length + mRatings.length + 3];
+
+                // Create a new Rating to allow the User to rate the Guide
+                mModels[mModels.length - 1] = new Rating();
+
+                // Copy the rest of the Ratings after the new Rating
+                System.arraycopy(mRatings, 0, mModels, mSections.length + 2, mRatings.length);
+            }
+        } else if (mRatings != null) {
+            mModels = new Object[mSections.length + mRatings.length + 2];
+
+            // Copy the Ratings to the end of the new Array if the user is not logged in
+            System.arraycopy(mRatings, 0, mModels, mSections.length + 2, mRatings.length);
+
+        } else {
+
+            mModels = new Object[mSections.length + 2];
+        }
+
+        // First item is the Guide's details
+        mModels[0] = mGuide;
+
+        // Copy the mSections Array into the middle sections of mModels
+        System.arraycopy(mSections, 0, mModels, 1, mSections.length);
+
+        // Set Author to come after Sections
+        mModels[mSections.length + 1] = mAuthor;
+
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Forces the Adapter to re-load its data
+     */
+    public void updateRating() {
+
+        updateModels();
     }
 
     public interface ClickHandler {
@@ -177,11 +285,13 @@ public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapte
 
     public class GuideDetailsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ViewDataBinding mBinding;
+        private GuideDetailsAdapter mAdapter;
 
-        public GuideDetailsViewHolder(ViewDataBinding binding) {
+        public GuideDetailsViewHolder(ViewDataBinding binding, GuideDetailsAdapter adapter) {
             super(binding.getRoot());
 
             mBinding = binding;
+            mAdapter = adapter;
 
             if (mBinding instanceof ListItemAuthorBinding) {
                 mBinding.getRoot().setOnClickListener(this);
@@ -197,26 +307,46 @@ public class GuideDetailsAdapter extends RecyclerView.Adapter<GuideDetailsAdapte
         }
 
         public void bind(int position) {
-            BaseModel model = mModels[position];
+            Object model = mModels[position];
 
             // Load the correct ViewModel into the ViewDataBinding based on the type of model for
             // the position
             if (model instanceof Guide) {
+
                 ((ListItemGuideDetailsBinding) mBinding).setVm(
                         // Pass in the MapboxActivity for lifecycle purposes
                         new GuideViewModel(mActivity, (Guide) model));
             } else if (model instanceof Section) {
+
                 // Load the correct ViewDataBinding depending on whether the section has an image
                 if (((Section) mModels[position]).hasImage) {
+
                     ((ListItemSectionImageBinding) mBinding).setVm(
                             new SectionViewModel((Section) model));
                 } else {
+
                     ((ListItemSectionTextBinding) mBinding).setVm(
                             new SectionViewModel((Section) model));
                 }
             } else if (model instanceof Author) {
+
                 ((ListItemAuthorBinding) mBinding).setVm(
                         new AuthorViewModel(mActivity, (Author) model));
+            } else if (model instanceof Rating) {
+
+                if (((Rating) model).getAuthorId() == null) {
+
+                    // If the Rating does not contain an AuthorId, it is a new Rating for the user
+                    // to rate the Guide with
+                    RatingViewModel vm = new RatingViewModel((Rating) model, mGuide, mAdapter, mUser);
+
+                    ((ListItemAddRatingBinding) mBinding).setVm(vm);
+                } else {
+
+                    RatingViewModel vm = new RatingViewModel((Rating) model, mGuide, mAdapter);
+
+                    ((ListItemRatingBinding) mBinding).setVm(vm);
+                }
             }
 
             // Immediately bind the data into the Views
