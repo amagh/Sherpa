5bc4ec94cec5bb5d07d90b78ca1b6a65d8d0d17e
Alvin <amagh1990@gmail.com>
2017-08-23 12:01:35 -0700

    Add Interface for storing request codes for UserFragment
    Will be used by both UserFragment for receiving the request and by
    AuthorViewModel for initiating the request.

---
+++ app/src/main/java/project/hikerguide/utilities/interfaces/UserFragmentRequestCodes.java
@@ -0,0 +1,10 @@
+package project.hikerguide.utilities.interfaces;
+
+/**
+ * Created by Alvin on 8/23/2017.
+ */
+
+public interface UserFragmentRequestCodes {
+    int REQUEST_CODE_PROFILE_PIC    = 5987;
+    int REQUEST_CODE_BACKDROP       = 1894;
+}
