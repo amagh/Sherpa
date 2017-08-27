6c767b57d5fc8200744e3e0fee3e6440e2345591
Alvin <amagh1990@gmail.com>
2017-08-27 03:08:51 -0700

    feat: Add DataCache class
    DataCache is used to store data models to a memory cache so that they
    can be retrieved from other Activities/Fragments. This reduces the
    number of network calls that need to be made as the data will already
    be stored in cache and will not need to be downloaded every time.
    
    This also gives the advantage of allowing user-initiated changes be
    immediately visible when returning from a different Activity.
    
    e.g. The user rates the guide, changing its rating and then returns to
    GuideListFragment. The change in rating is immediately seen as the data
    was changed in the cached Object which is then used to populate the
    Adapter.

---
+++ app/src/main/java/project/hikerguide/utilities/DataCache.java
@@ -0,0 +1,110 @@
+package project.hikerguide.utilities;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+
+import project.hikerguide.models.datamodels.Section;
+import project.hikerguide.models.datamodels.abstractmodels.BaseModel;
+
+/**
+ * Created by Alvin on 8/26/2017.
+ */
+
+public class DataCache {
+
+    // ** Member Variables ** //
+    private static DataCache sDataCache = new DataCache();
+    private HashMap<String, WeakReference<BaseModel>> mDataMap;
+    private HashMap<String, WeakReference<Section[]>> mArrayMap;
+
+    private DataCache() {
+        mDataMap = new HashMap<>();
+        mArrayMap = new HashMap<>();
+    }
+
+    /**
+     * Enforces Singleton Pattern.
+     *
+     * @return The Singleton instance of the DataCache
+     */
+    public static DataCache getInstance() {
+        return sDataCache;
+    }
+
+    /**
+     * Stores a data model in the DataCache
+     *
+     * @param model    The data model to be cached
+     */
+    public void store(BaseModel model) {
+
+        // Create a WeakReference to the data
+        WeakReference<BaseModel> dataReference = new WeakReference<>(model);
+
+        // Add the WeakReference to the DataCache
+        mDataMap.put(model.firebaseId, dataReference);
+    }
+
+    /**
+     * Stores an Array of Sections in the DataCache
+     *
+     * @param sections    The Array of Sections to be cached
+     */
+    public void store(Section[] sections) {
+
+        // Get a reference to one of the Sections for the guide's firebaseId
+        Section section = sections[0];
+
+        // Create a WeakReference to the Array
+        WeakReference<Section[]> arrayReference = new WeakReference<>(sections);
+
+        // Cache the Array
+        mArrayMap.put(section.guideId, arrayReference);
+    }
+
+    /**
+     * Retrieves a data model from the cache
+     *
+     * @param firebaseId    The FirebaseId used as the key to the data model from the cache
+     * @return The data model associated with the FirebaseId
+     */
+    public BaseModel get(String firebaseId) {
+
+        // Attempt to retrieve the data from cache
+        WeakReference<BaseModel> dataReference = mDataMap.get(firebaseId);
+
+        // Check to ensure the data is valid
+        if (dataReference != null && dataReference.get() != null) {
+
+            // Return the data from the WeakReference
+            return dataReference.get();
+        } else {
+
+            // Does not exist in the cache. Return null
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves an Array of Sections from the cache
+     *
+     * @param guideId    The FirebaseId of the Guide associated with the Sections used as the key
+     * @return An Array of Sections corresponding to the FirebaseId of the Guide they belong to
+     */
+    public Section[] getSections(String guideId) {
+
+        // Attempt to retrieve the data from cache
+        WeakReference<Section[]> sectionReference = mArrayMap.get(guideId);
+
+        // Check to ensure the data is valid
+        if (sectionReference != null && sectionReference.get() != null) {
+
+            // Return the data from the WeakReference
+            return sectionReference.get();
+        } else {
+
+            // Does not exist in the cache. Return null
+            return null;
+        }
+    }
+}
