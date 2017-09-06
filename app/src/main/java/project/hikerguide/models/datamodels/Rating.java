d6be1b742f9eccfc1a976a65bd5cee7a85e767f9
Alvin <amagh1990@gmail.com>
2017-09-06 13:23:54 -0700

    fix: Fix crash when loading guide details
    When loading the ratings for the guide, there was a mistake where the
    Rating getGuideId() was annotated with @Exclude so the value wasn't
    added to the Rating when it was downloaded from Firebase Database.
    
    This caused a NPE as it tried to compare a null guideId.
    
    Fixed the issue by removed the @Exclude annotation.

--- app/src/main/java/project/hikerguide/models/datamodels/Rating.java
+++ app/src/main/java/project/hikerguide/models/datamodels/Rating.java
@@ -64,7 +64,6 @@ public class Rating extends BaseModel {
     //*********************************** Getters and Setters ************************************//
     //********************************************************************************************//
 
-    @Exclude
     public String getGuideId() {
         return guideId;
     }
