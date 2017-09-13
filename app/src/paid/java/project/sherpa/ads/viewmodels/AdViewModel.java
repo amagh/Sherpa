3354c0820f63d54319fe9ea37199822a2ec316f8
Alvin <amagh1990@gmail.com>
2017-09-13 12:30:28 -0700

    fix: Fix compilation error for paid flavor
    The paid flavor did not include an AdViewModel, causing the the app to
    be uncompilable.
    
    Added a dummy class to satisfy the requirement.

---
+++ app/src/paid/java/project/sherpa/ads/viewmodels/AdViewModel.java
@@ -0,0 +1,13 @@
+package project.sherpa.ads.viewmodels;
+
+import android.app.Activity;
+import android.databinding.BaseObservable;
+
+/**
+ * Dummy class for parity with free version
+ */
+
+public class AdViewModel extends BaseObservable {
+
+    public AdViewModel(Activity activity) {}
+}
