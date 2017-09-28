29ed24e39636e4ac828594e501dcd21217c3f4d7
Alvin <amagh1990@gmail.com>
2017-09-28 13:37:29 -0700

    feat: Update FriendFollowActivity
    Updated it to use SmartValueEventListener and continuously listen for
    changes to the Author. This allows it to update the ViewModel when
    changes are detected.

--- app/src/main/java/project/sherpa/ui/activities/FriendFollowActivity.java
+++ app/src/main/java/project/sherpa/ui/activities/FriendFollowActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 import project.sherpa.BR;
 import project.sherpa.R;
@@ -13,21 +15,24 @@ import project.sherpa.models.datamodels.Author;
 import project.sherpa.models.datamodels.abstractmodels.BaseModel;
 import project.sherpa.models.viewmodels.FriendFollowViewModel;
 import project.sherpa.utilities.DataCache;
-import project.sherpa.utilities.FirebaseProviderUtils;
+import project.sherpa.utilities.objects.SmartValueEventListener;
 import timber.log.Timber;
 
 import static project.sherpa.utilities.Constants.IntentKeys.AUTHOR_KEY;
+import static project.sherpa.utilities.FirebaseProviderUtils.FirebaseType.AUTHOR;
 
 /**
  * Created by Alvin on 9/27/2017.
  */
 
-public class FriendFollowActivity extends ConnectivityActivity {
+public class FriendFollowActivity extends ConnectivityActivity implements ConnectivityActivity.ConnectivityCallback{
 
     // ** Member Variables ** //
     private ActivityFriendFollowBinding mBinding;
     private Author mSendUser;
     private Author mReceiveUser;
+    private FriendFollowViewModel mViewModel;
+    private SmartValueEventListener[] listeners = new SmartValueEventListener[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,8 @@ public class FriendFollowActivity extends ConnectivityActivity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_friend_follow);
 
         if (getIntent() == null) finish();
+
+        addConnectivityCallback(this);
 
         // Start the process that will load the ViewModel
         loadViewModel();
@@ -45,6 +52,36 @@ public class FriendFollowActivity extends ConnectivityActivity {
         // Load the user profile for the user being accessed
         String userId = getIntent().getStringExtra(AUTHOR_KEY);
         loadReceiveUserProfile(userId);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Start listening for changes
+        for (SmartValueEventListener listener : listeners) {
+            if (listener != null) listener.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Stop listening for changes
+        for (SmartValueEventListener listener : listeners) {
+            if (listener != null) listener.stop();
+        }
+    }
+
+    @Override
+    public void onConnected() {
+        FirebaseDatabase.getInstance().goOnline();
+    }
+
+    @Override
+    public void onDisconnected() {
+        FirebaseDatabase.getInstance().goOffline();
     }
 
     /**
@@ -65,21 +102,30 @@ public class FriendFollowActivity extends ConnectivityActivity {
         // Attempt to retrieve the user from the cache
         mSendUser = (Author) DataCache.getInstance().get(user.getUid());
 
-        if (mSendUser == null) {
+        if (listeners[0] == null) {
+            // Author does not exist in the cache, load it from Firebase
+            SmartValueEventListener listener = new SmartValueEventListener(AUTHOR, user.getUid()) {
+                @Override
+                public void onModelChange(BaseModel model) {
+                    if (model == null) return;
+                    DataCache.getInstance().store(model);
 
-            // Retrieve the user from Firebase
-            FirebaseProviderUtils.getModel(FirebaseProviderUtils.FirebaseType.AUTHOR, user.getUid(),
-                    new FirebaseProviderUtils.FirebaseListener() {
-                        @Override
-                        public void onModelReady(BaseModel model) {
-                            if (model == null) return;
-                            DataCache.getInstance().store(model);
+                    loadSendUserProfile();
+                }
+            };
 
-                            loadSendUserProfile();
-                        }
-                    });
-        } else {
+            listener.start();
+            listeners[0] = listener;
+        }
+
+        if (mSendUser != null && mViewModel == null) {
+
+            // ViewModel not instantiated, notify the Thread that mSendUser is loaded
             notifyAll();
+        } else if (mSendUser != null) {
+
+            // ViewModel is loaded, notify it to update its values
+            mViewModel.notifyPropertyChanged(BR._all);
         }
     }
 
@@ -93,21 +139,30 @@ public class FriendFollowActivity extends ConnectivityActivity {
         // Attempt to retrieve the user from the cache
         mReceiveUser = (Author) DataCache.getInstance().get(userId);
 
-        if (mReceiveUser == null) {
+        if (listeners[1] == null) {
+            // Author does not exist in the cache, load it from Firebase
+            SmartValueEventListener listener = new SmartValueEventListener(AUTHOR, userId) {
+                @Override
+                public void onModelChange(BaseModel model) {
+                    if (model == null) return;
+                    DataCache.getInstance().store(model);
 
-            // Retrieve the user from Firebase
-            FirebaseProviderUtils.getModel(FirebaseProviderUtils.FirebaseType.AUTHOR, userId,
-                    new FirebaseProviderUtils.FirebaseListener() {
-                        @Override
-                        public void onModelReady(BaseModel model) {
-                            if (model == null) return;
-                            DataCache.getInstance().store(model);
+                    loadReceiveUserProfile(userId);
+                }
+            };
 
-                            loadReceiveUserProfile(userId);
-                        }
-                    });
-        } else {
+            listener.start();
+            listeners[1] = listener;
+        }
+
+        if (mReceiveUser != null && mViewModel == null) {
+
+            // ViewModel not instantiated, notify the Thread that mReceiveUser is loaded
             notifyAll();
+        } else if (mReceiveUser != null) {
+
+            // ViewModel is loaded, notify it to update its values
+            mViewModel.notifyPropertyChanged(BR._all);
         }
     }
 
@@ -134,10 +189,9 @@ public class FriendFollowActivity extends ConnectivityActivity {
                     }
 
                     // Init and set the ViewModel
-                    FriendFollowViewModel vm =
-                            new FriendFollowViewModel(FriendFollowActivity.this, mSendUser, mReceiveUser);
+                    mViewModel = new FriendFollowViewModel(FriendFollowActivity.this, mSendUser, mReceiveUser);
 
-                    mBinding.setVm(vm);
+                    mBinding.setVm(mViewModel);
                     mBinding.notifyPropertyChanged(BR._all);
                 }
             }
