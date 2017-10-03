25f48af75a9c1805e2738afc3040ae017d7c5b7f
Alvin <amagh1990@gmail.com>
2017-10-03 14:46:09 -0700

    feat: Add QueryChangeListener
    Serves the same role as ModelChangeListener but for Queries.

---
+++ app/src/main/java/project/sherpa/services/firebaseservice/QueryChangeListener.java
@@ -0,0 +1,63 @@
+package project.sherpa.services.firebaseservice;
+
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import project.sherpa.models.datamodels.abstractmodels.BaseModel;
+import project.sherpa.utilities.FirebaseProviderUtils;
+
+/**
+ * Created by Alvin on 10/3/2017.
+ */
+
+public abstract class QueryChangeListener<T extends BaseModel> {
+
+    // ** Member Variables ** //
+    @FirebaseProviderUtils.FirebaseType
+    private int mType;
+    private Query mQuery;
+    private T[] mModels;
+
+    public QueryChangeListener(@FirebaseProviderUtils.FirebaseType int type, Query query) {
+        mType = type;
+        mQuery = query;
+    }
+
+    /**
+     * Notifies the observer that the underlying data has changed
+     *
+     * @param models    An Array of BaseModels corresponding to the data returned by the Query
+     */
+    void updateModels(T[] models) {
+        mModels = models;
+        onQueryChanged(mModels);
+    }
+
+    /**
+     * Called when the data being queried is updated
+     *
+     * @param models    An Array of BaseModels corresponding to the data returned by the Query
+     */
+    public abstract void onQueryChanged(T[] models);
+
+    //********************************************************************************************//
+    //*********************************** Getters & Setters **************************************//
+    //********************************************************************************************//
+
+    @FirebaseProviderUtils.FirebaseType
+    public int getType() {
+        return mType;
+    }
+
+    public Query getQuery() {
+        return mQuery;
+    }
+
+    public String getUriString() {
+        return mQuery.toString();
+    }
+
+
+}
