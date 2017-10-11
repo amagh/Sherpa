f888e9d555a32bc23bb136fdafe5a59385a9dfef
Alvin <amagh1990@gmail.com>
2017-10-10 19:30:30 -0700

    feat: Update Rating data model
    Added methods for updating the Rating's Firebase entries as well as the
    associated guide and author's scores.

--- app/src/main/java/project/sherpa/models/datamodels/Rating.java
+++ app/src/main/java/project/sherpa/models/datamodels/Rating.java
@@ -1,12 +1,20 @@
 package project.sherpa.models.datamodels;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.Transaction;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import project.sherpa.data.GuideDatabase;
 import project.sherpa.models.datamodels.abstractmodels.BaseModel;
+import timber.log.Timber;
 
 /**
  * Created by Alvin on 8/18/2017.
@@ -51,7 +59,11 @@ public class Rating extends BaseModel {
     public boolean equals(Object obj) {
 
         if (obj instanceof Rating) {
-            if (((Rating) obj).getComment().equals(this.comment) &&
+            if (((Rating) obj).getComment() == null && this.comment != null ||
+                    ((Rating) obj).getComment() != null && this.comment == null) {
+                return false;
+            } else if ((((Rating) obj).getComment() == (this.comment) ||
+                    ((Rating) obj).getComment().equals(this.comment)) &&
                     ((Rating) obj).rating == this.rating) {
                 return true;
             }
@@ -84,6 +96,126 @@ public class Rating extends BaseModel {
         dateAdded       = newRatingValues.dateAdded;
     }
 
+    /**
+     * Updates the Rating on Firebase Database
+     *
+     * @param previousRating    The previous numerical rating. 0 if it hasn't been rated before
+     */
+    public void updateInFirebase(final int previousRating) {
+
+        // Set the FirebaseId of the Rating if it is new
+        if (firebaseId == null) {
+            firebaseId = FirebaseDatabase.getInstance().getReference()
+                    .child(DIRECTORY).child(guideId).push().getKey();
+        }
+
+        // Set the Rating to add the Server timestamp as the date
+        addDate();
+
+        // Get the directory for where to push the update
+        String directory = DIRECTORY + "/" + guideId + "/" + firebaseId;
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(directory, toMap());
+
+        // Update the Firebase entry
+        FirebaseDatabase.getInstance().getReference().updateChildren(childUpdates)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        // Update guide and author scores
+                        updateGuideScore(previousRating);
+                        updateAuthorScore(previousRating);
+                    }
+                });
+    }
+
+    /**
+     * Updates the score of the Guide for which the rating is for
+     *
+     * @param previousRating    The previous numerical rating this Rating was changed from
+     */
+    private void updateGuideScore(final int previousRating) {
+
+        Transaction.Handler guideUpdateHandler = new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                Guide guide = mutableData.getValue(Guide.class);
+
+                if (guide == null) {
+                    // Glitch -- recall the function
+                    updateGuideScore(previousRating);
+                    return Transaction.abort();
+                }
+
+                // Update guide's values
+                guide.rating = guide.rating - previousRating + rating;
+                if (previousRating == 0) {
+                    guide.reviews++;
+                }
+
+                mutableData.setValue(guide.toMap());
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+                if (databaseError != null) {
+                    Timber.e("Error updating guide score\n" + databaseError.getMessage());
+                }
+            }
+        };
+
+        FirebaseDatabase.getInstance().getReference()
+                .child(GuideDatabase.GUIDES)
+                .child(guideId)
+                .runTransaction(guideUpdateHandler);
+    }
+
+    /**
+     * Updates the score of the Author for which the Rating corresponds to
+     * @param previousRating    The previous numerical Rating given to the Author
+     */
+    private void updateAuthorScore(final int previousRating) {
+
+        // Ratings less than three do not affect the Author's score. This is to prevent many low
+        // ratings from giving bad authors a benefit.
+        if (rating < 3 && previousRating < 3) return;
+
+        Transaction.Handler authorUpdateHandler = new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                Author author = mutableData.getValue(Author.class);
+
+                if (author == null) {
+                    // Glitch -- recall the function
+                    updateAuthorScore(previousRating);
+                    return Transaction.abort();
+                }
+
+                // Update author values
+                if (rating >= 3) author.score += rating;
+                if (previousRating >= 3) author.score -= previousRating;
+
+                mutableData.setValue(author.toMap());
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+                if (databaseError != null) {
+                    Timber.e("Error updating author's score\n" + databaseError.getMessage());
+                }
+            }
+        };
+
+        FirebaseDatabase.getInstance().getReference()
+                .child(GuideDatabase.AUTHORS)
+                .child(guideAuthorId)
+                .runTransaction(authorUpdateHandler);
+    }
+
     //********************************************************************************************//
     //*********************************** Getters and Setters ************************************//
     //********************************************************************************************//
@@ -92,7 +224,6 @@ public class Rating extends BaseModel {
         return guideId;
     }
 
-    @Exclude
     public String getGuideAuthorId() {
         return guideAuthorId;
     }
